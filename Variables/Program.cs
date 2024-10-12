dcb2fa979d51348d0cb08c746989d072173e8af8
pkaselj <petar.kaselj.00@gmail.com>
2024-10-12 16:15:50 +0200

    Added second example

---
+++ Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Variable declaration and initialization at the same time
+        int final_grade = 5;
+        double average_grade = 4.6;
+
+        // Variable declaration then initialization
+        string name;
+        name = "Ivan";
+
+        // String interpolation - combine strings
+        string message = $"Hello, {name}! Your average grade was: {average_grade}. ";
+        message += $"Your final grade is {final_grade}!";
+
+
+        Console.WriteLine(message);
+
+        // You can change value of each variable (unless it's `const` or `readonly`)
+        name = "Marija";
+        final_grade = 5;
+        average_grade = 4.8;
+        message = $"Hello, {name}! Your average grade was: {average_grade}. ";
+        message += $"Your final grade is {final_grade}!";
+        Console.WriteLine(message);
+    }
+}
