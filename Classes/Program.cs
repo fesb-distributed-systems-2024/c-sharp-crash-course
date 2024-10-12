c2d84cca3776442ad8d3c276fd6ccab7899844be
pkaselj <petar.kaselj.00@gmail.com>
2024-10-12 16:39:27 +0200

    Added another exercise

---
+++ Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Classes;
+
+class Student
+{
+    // Private student fields - not accessible from outside of class
+    private string name;
+    private string surname;
+    private double grade;
+
+    // Constructor
+    public Student(string name, string surname, double grade)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.grade = grade;
+    }
+
+    // Public functions - accessible from outside
+    public double GetGrade()
+    {
+        return grade;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, {name} {surname}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student("Ivana", "Ivanovic", 4.8);
+        student1.Greet();
+
+        double grade = student1.GetGrade();
+        Console.WriteLine($"Student has grade: {grade}");
+    }
+}
---
+++ FlowControl/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FlowControl;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int limit = 20;
+
+        Console.WriteLine($"Counting from {limit} to 0");
+
+        while(limit > 0)
+        {
+            Console.Write($"I'm at number {limit}");
+            
+            if(limit % 2 == 0)
+            {
+                Console.WriteLine(" - which is an even number");
+            }
+            else
+            {
+                Console.WriteLine(" - which is an odd number");
+            }
+
+            limit -= 1;
+        }
+
+        Console.WriteLine("Finished counting!");
+    }
+}
