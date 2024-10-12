8bad133136cd2f6fb7ffa44e9fc8da8ee83f867c
pkaselj <petar.kaselj.00@gmail.com>
2024-10-12 16:22:05 +0200

    Added third example - arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] students = {"Ante", "Mate", "Ivana", "Mare"};
+
+        Console.WriteLine($"Number of students is { students.Length }");
+        Console.WriteLine($"First student is: { students[0] }");
+        Console.WriteLine($"Last student is: { students[students.Length - 1] }");
+
+        // Looping through all students using for-loop
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine($"Hello, student {students[i]}");
+        }
+
+        // Looping through all students using foreach-loop
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Hello again, {student}");
+        }
+    }
+}
